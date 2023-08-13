b1c1e387894f94f2fdb5d1ee141858964dbe8929
Sabin Thapa <112601289+Sabinkthapa@users.noreply.github.com>
2023-08-13 03:07:18 -0300

    Completing the password generator with all the comments for user information

--- script.js
+++ script.js
@@ -5,29 +5,61 @@ selects the elements with id generate from the dom and
 store in the generatebtm variable */
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// 
 function writePassword() {
+  // call the generatepassword function to generate the password
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // find the html element with password id
+  var passwordText = document.querySelector("#password"); 
+  // set the value of html element to the password
   passwordText.value = password;
 }
 
 function generatePassword() {
   // we need to prompt the series of criteria for password like length between 8-128, uppercase,lowercase, special characters
-  const length =prompt ("please enter the password length between 8-128");
-  // if condition to check to length validity from user 
-  if (length < 8 || length > 128)
-  alert("Please enter the valid password length");
-  return "";
+  // parseint convert the string value from user to integer value
+  const length =parseInt(prompt("please enter the password length between 8-128"));
 
-  // we use confirm() function to ask user for uppercase, lowercase, special characters in the passord
-  const uppercase_inthepassword = confirm("Do you want upper case in your password?");
-  const lowercase_inthepassword = confirm("Do you want lower case in your password?");
-  const specialcharater_inthepassword = confirm("Do you want special char in your password?");
-  const Number_inthepassword = confirm("Do you want number in your password?");
+  // if condition to check to length validity from user,isNan check the validity of the input value.
+  if (isNaN(length) || length < 8 || length > 128){
+    alert("Please enter the valid password length");
+    return "";
+  }
 
+  // we use confirm() method to ask user for uppercase, lowercase, special characters with dialog box in the window.
+
+  const includeuppercase = confirm ("Do you want upper case in your password?");
+  const includelowercase = confirm ( "Do you want lower case in your password?");
+  const includespecial = confirm ("Do you want special char in your password?");
+  const includeNumber = confirm ("Do you want number in your password?");
+  
+// verify for atleast one character type for the password
+  if(!includeuppercase && !includelowercase && !includespecial && !includeNumber) {
+    alert ("Please select atleast one character type");
+    return "";
+  }
+  // assigning const variables for types of characters
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const numberChars = "0123456789";
+  const specialChars = "!@#$%^&*()-_=+";
+
+  // concaneate the specfic character set if the condition is met,intial value as empty string
+  let all= "";
+  if (includeuppercase) all += uppercaseChars;
+  if (includelowercase) all += lowercaseChars;
+  if (includespecial) all += specialChars;
+  if (includeNumber) all += numberChars;
+
+  // math.random generate random floating point numbers that scaled the range of the indices in the all string ,
+  // while math.floor converts into integer number,password store the value ,charAt method extract the character at specfic index
+  // and the password is stored and return.
+  let password = "";
+  for (let i=0; i<length; i++) {
+    const randindex = Math.floor(Math.random()* all.length);
+    password += all.charAt(randindex);
+  }
+  return password;
 }
-
-
 // Add event listener to generate button and the function executed when clicked
 generateBtn.addEventListener("click", writePassword);
